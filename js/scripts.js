370a4bb478eefb3df175a901e8ce4aafe1d3ea1f
gabit <gabit690@gmail.com>
2020-07-08 15:48:16 -0300

    reproduccion de audios y animacion de fondo Hawk

--- js/scripts.js
+++ js/scripts.js
@@ -8,4 +8,53 @@ window.addEventListener('load', function(e) {
         info.classList.toggle('activo');
     });
 
+    // Funcionalidad para la reproduccion de audios
+
+    const botonIntro = document.querySelector('#dexter-cabeza');
+    const frente = document.getElementById('fondo-hawk');
+    const botonHawk = document.querySelector('#diskette');
+    const intro = document.querySelector('#intro');
+    const hawk = document.querySelector('#hawk');
+    
+    botonIntro.addEventListener('click', (e) => {
+        if (hawk.currentTime != 0) {
+            hawk.pause();
+            hawk.currentTime = 0;
+        }
+        if (intro.currentTime == 0) {
+            intro.play();
+        } else {
+            intro.pause();
+            intro.currentTime = 0;
+        }
+    });
+    
+    botonHawk.addEventListener('click', (e) => {
+        if (intro.currentTime != 0) {
+            intro.pause();
+            intro.currentTime = 0;
+        }
+        if (hawk.currentTime == 0) {
+            hawk.play();
+            // Animacion del fondo Hawk.
+            frente.style.display = 'flex';
+            let opacidad = 1;
+            let cambio = setInterval((e) => {
+                if (opacidad > 0.05) {
+                    opacidad -= 0.05;
+                    frente.style.opacity = opacidad;
+                } else {
+                    clearInterval(cambio);
+                }
+            }, 200);
+            setTimeout((e) => {
+                frente.style.display = 'none';
+            }, 4000);
+
+        } else {
+            hawk.pause();
+            hawk.currentTime = 0;
+        }
+    });
+
 });
